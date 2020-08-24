2b7e3f8837826d1d0043f56782896383de8bf0b1
akazad1235 <akazad914@gmail.com>
2020-08-24 15:45:22 +0600

    Render Multiple Component in an array

--- src/App.js
+++ src/App.js
@@ -37,6 +37,9 @@ function App() {
           }
       <Counter></Counter>
       <Example></Example>
+      <Movies></Movies>
+      <Students></Students>
+      
       </header>    
     </div>
   );
@@ -102,5 +105,52 @@ function Example() {
     </div>
   );
 }
+
+function Movies(){
+
+    const [movie, setMovie] = useState(5)
+
+
+  return (
+    <div>
+      <h1>Movies:{movie}</h1>
+      <OthersMovies movies ={movie+3}></OthersMovies>
+      <button onClick={() => setMovie(movie +1)}>Add Movie</button>
+    </div>
+  )
+}
+
+function OthersMovies(props){
+
+return <h1>Movie-2 : {props.movies}</h1>
+
+}
+
+function Students(){
+    const studentsObj = [{name:'Azad', roll:11, age:23}, {name:'Tarikul', roll:25, age:26},  {name:'Tarikul', roll:25, age:26} ];
+
+
+    return (
+      <div>
+        {
+          studentsObj.map(std => <Schools name={std.name} roll={std.roll}></Schools>)
+        }
+      </div>
+    )
+}
+
+function Schools(props){
+    
+  return(
+    <div style={{border:'5px solid #dddd', width:'100%', margin:'10px'}}>
+      <h5>Student Name:{props.name}</h5>
+      <h5>Studet Roll: {props.roll}</h5>
+      <h5>Student Age: {props.age}</h5>
+    </div>
+  )
+
+}
+
+
 
 export default App;
